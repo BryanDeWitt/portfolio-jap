130980d50dc2c43019bfed64b573717ec4421ed6
Bryan <tolovehayley@gmail.com>
2025-08-06 12:58:26 -0300

    todo agrago para enviar la tarea, video de fondo imagen de ingeniero y favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import cheemsImg from './assets/cheems.jpeg'
+import videoFondo from './assets/construccion.mp4'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Bryan Luzardo</h1>
+      <img src={cheemsImg} alt="cheems imgeniero" />
+      <h2 className='construccion uno'>Sitio en construccion</h2>
+      <h2 className='construccion dos'>Solo para probar tarea</h2>
+      <video loop autoPlay muted src={videoFondo}></video>
     </>
   )
 }
